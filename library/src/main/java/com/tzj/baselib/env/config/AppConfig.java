91c58ab9470e5bd1373be6e84ee31a363fa25d54
tangzejin921 <tangzejin921@qq.com>
2019-03-22 10:56:42 +0800

    去除没必要的常量

--- library/src/main/java/com/tzj/baselib/env/config/AppConfig.java
+++ library/src/main/java/com/tzj/baselib/env/config/AppConfig.java
@@ -1,10 +1,5 @@
 package com.tzj.baselib.env.config;
 
-import android.content.Context;
-
-import com.tzj.baselib.R;
-import com.tzj.baselib.env.AppEnv;
-
 /**
  * 静态配置
  */
@@ -20,52 +15,9 @@ public class AppConfig {
         }
     }
 
-    private static Context getAppContext() {
-        return AppEnv.getAppCtx();
-    }
-
     public static boolean isAndroid(){
         return isAndroid;
     }
 
-    /**
-     * 获取产品ID
-     *
-     * @return productId
-     */
-    public static String getProductId() {
-        return getAppContext().getString(R.string.productId);
-    }
-
-    /**
-     * 获取医院编码
-     *
-     * @return hospitalCode
-     */
-    public static String getHosCode() {
-        return getAppContext().getString(R.string.hosCode);
-    }
-
-    public static String getHosName() {
-        return getAppContext().getString(R.string.hosName);
-    }
-
-    public static String getAreaCode() {
-        return getAppContext().getString(R.string.areaCode);
-    }
-
-    public static String getAliYunKey() {
-        return getAppContext().getString(R.string.aliyun_key);
-    }
-
-    public static String getAliYunSecret() {
-        return getAppContext().getString(R.string.aliyun_secret);
-    }
-    public static String getSVersion(){
-        return "1.00.01";
-    }
-    public static String getPltID(){
-        return "02";
-    }
 
 }

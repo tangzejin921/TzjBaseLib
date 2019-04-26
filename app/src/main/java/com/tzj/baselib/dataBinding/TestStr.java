754de9fb0e68de2b5f82f2fa7b3a95716415c719
tangzejin921 <tangzejin921@qq.com>
2019-04-26 13:47:32 +0800

    更新 TzjRecyclerView 支持 databinding

---
+++ app/src/main/java/com/tzj/baselib/dataBinding/TestStr.java
@@ -0,0 +1,20 @@
+package com.tzj.baselib.dataBinding;
+
+
+/**
+ * Copyright © 2019 健康无忧网络科技有限公司<br>
+ * Author:      唐泽金 tangzejin921@qq.com<br>
+ * Version:     1.0.0<br>
+ * Date:        2019-04-24 18:09<br>
+ * Description: TODO 请输入此类的功能
+ */
+public class TestStr {
+    private String test;
+
+    public String getTest() {
+        return test;
+    }
+    public void setTest(String test) {
+        this.test = test;
+    }
+}
